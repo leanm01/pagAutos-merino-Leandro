0bdcf8601038df9e4e0c5454608b3132416b6f58
leandro merino <60607751+leanm01@users.noreply.github.com>
2022-05-21 12:14:48 -0300

    act

---
+++ extension.js
@@ -0,0 +1,13 @@
+let ingreseNombre = prompt("Ingrese su nombre y apellido: ");
+
+if(ingreseNombre != 0)
+{
+    alert("Se ha ingresado el nombre",ingreseNombre);
+    confirm("Disfrute de la pagina!!!");
+}
+else
+{
+    alert("No se ha ingresado el nombre",ingreseNombre);
+}
+
+
